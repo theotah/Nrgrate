ccac12f8ebdc9db1fc5f6d3b5ad0da3821a34a43
Theo Tahmazis <theo.tahmazis@optusnet.com.au>
2019-05-13 03:45:34 +0000

    completed styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ def create
 
 if @article.save
 
-flash[:notice] = "Article was successfully created"
+flash[:success] = "Article was successfully created"
 
 redirect_to article_path(@article)
 
@@ -40,7 +40,7 @@ def update
 
 if @article.update(article_params)
 
-flash[:notice] = "Article was successfully updated"
+flash[:success] = "Article was successfully updated"
 
 redirect_to article_path(@article)
 
@@ -61,7 +61,7 @@ def destroy
 
 @article.destroy
 
-flash[:notice] = "Article was successfully deleted"
+flash[:success] = "Article was successfully deleted"
 
 redirect_to articles_path
 
